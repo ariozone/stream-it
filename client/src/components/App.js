3873bd7624b6867b4f6ae417e8bab9e52c22fe4c
Ario Tamaddoni <ariozone6@gmail.com>
2019-08-19 17:52:39 -0700

    Created routes for each component.#1

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,24 @@
 import React from "react"
 import { BrowserRouter, Route } from "react-router-dom"
+import StreamList from "./streams/StreamList"
+import CreateStream from "./streams/CreateStream"
+import DeleteStream from "./streams/DeleteStream"
+import EditStream from "./streams/EditStream"
+import ShowStream from "./streams/ShowStream"
 import "../App.css"
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <h1>Stream It!</h1>
+        <div>
+          <h1>Stream It!</h1>
+          <Route path='/' exact component={StreamList} />
+          <Route path='/streams/new' component={CreateStream} />
+          <Route path='/streams/delete' component={DeleteStream} />
+          <Route path='/streams/edit' component={EditStream} />
+          <Route path='/streams/show' component={ShowStream} />
+        </div>
       </BrowserRouter>
     </div>
   )
