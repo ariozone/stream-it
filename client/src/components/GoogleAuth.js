603eb7e535ca092e5675337142f00a2d8e93a33e
Ario Tamaddoni <ariozone6@gmail.com>
2019-08-20 18:45:11 -0700

    Enhanced GoogleAuth component to initialize google authentication when the component mounts.#2

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,6 +1,19 @@
 import React from "react"
+require("dotenv").config()
 
 export default class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId: process.env.REACT_APP_CLIENT_ID,
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance()
+        })
+    })
+  }
   render() {
     return <h1>Google Auth</h1>
   }
