3a7c9e0531b93f37b338307b04042c573529b3fd
Ario Tamaddoni <ariozone6@gmail.com>
2019-08-21 17:10:37 -0700

    Setup redux.#3

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./index.css"
+import { Provider } from "react-redux"
+import { createStore } from "redux"
+
 import App from "./components/App"
+import reducers from "./reducers"
+import "./index.css"
 require("dotenv").config()
 
-ReactDOM.render(<App />, document.getElementById("root"))
+const store = createStore(reducers)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+)
