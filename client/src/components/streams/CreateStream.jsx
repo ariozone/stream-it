ecfed491bf8f12cc144f052af8993f331276cf4c
Ario Tamaddoni <ariozone6@gmail.com>
2019-08-26 18:56:18 -0700

    Implemented validation.#3

--- client/src/components/streams/CreateStream.jsx
+++ client/src/components/streams/CreateStream.jsx
@@ -2,11 +2,13 @@ import React from "react"
 import { Field, reduxForm } from "redux-form"
 
 class CreateStream extends React.Component {
-  renderInput = ({ input, label }) => {
+  renderInput = ({ input, label, meta }) => {
+    console.log(meta)
     return (
       <div className='field'>
         <label htmlFor={input.name}>{label}</label>
         <input {...input} />
+        <div>{meta.error}</div>
       </div>
     )
   }
@@ -33,10 +35,14 @@ class CreateStream extends React.Component {
 }
 const validate = formValues => {
   const errors = {}
+
   if (!formValues.title) errors.title = "Title is required!"
   if (!formValues.description) errors.description = "Description is required!"
+
   return errors
 }
+
 export default reduxForm({
-  form: "CreateStream"
+  form: "CreateStream",
+  validate
 })(CreateStream)
