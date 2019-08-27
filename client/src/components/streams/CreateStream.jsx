8a2d60b4ddd5179f1733c073ffe57527583b256a
Ario Tamaddoni <ariozone6@gmail.com>
2019-08-26 19:20:55 -0700

    Enhanced validation to show the error message after user's first interaction.#3

--- client/src/components/streams/CreateStream.jsx
+++ client/src/components/streams/CreateStream.jsx
@@ -2,13 +2,21 @@ import React from "react"
 import { Field, reduxForm } from "redux-form"
 
 class CreateStream extends React.Component {
+  renderError = ({ error, touched }) => {
+    if (touched && error)
+      return (
+        <div className='ui error message'>
+          <div className='header '>{error}</div>
+        </div>
+      )
+  }
   renderInput = ({ input, label, meta }) => {
     console.log(meta)
     return (
-      <div className='field'>
+      <div className={`field + ${meta.error && meta.touched ? "error" : ""}`}>
         <label htmlFor={input.name}>{label}</label>
-        <input {...input} />
-        <div>{meta.error}</div>
+        <input {...input} autoComplete='off' />
+        {this.renderError(meta)}
       </div>
     )
   }
@@ -19,7 +27,7 @@ class CreateStream extends React.Component {
   render() {
     return (
       <form
-        className='ui form'
+        className='ui form error'
         onSubmit={this.props.handleSubmit(this.onSubmit)}
       >
         <Field name='title' component={this.renderInput} label='Enter Title' />
