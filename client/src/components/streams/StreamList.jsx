c798c32eb28018d712df1398e2ade1bdd70c9235
Ario Tamaddoni <ariozone6@gmail.com>
2019-08-29 18:30:00 -0700

    Improved elements presentation.#4

--- client/src/components/streams/StreamList.jsx
+++ client/src/components/streams/StreamList.jsx
@@ -11,8 +11,11 @@ class StreamList extends Component {
       return (
         <div className='item' key={stream.id}>
           <i className='large middle aligned icon camera' />
-          <div className='content'>{stream.title}</div>
-          <div className='description'>{stream.description}</div>
+
+          <div className='content'>
+            {stream.title}
+            <div className='description'>{stream.description}</div>
+          </div>
         </div>
       )
     })
