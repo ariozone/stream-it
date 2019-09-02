902b2f257f01f2d9e06dd47ae4c0588815e80295
Ario Tamaddoni <ariozone6@gmail.com>
2019-09-02 13:33:26 -0700

    Enhanced EditStream component to render the content of the selected stream.#5

--- client/src/components/streams/EditStream.jsx
+++ client/src/components/streams/EditStream.jsx
@@ -1,7 +1,20 @@
 import React from "react"
+import { connect } from "react-redux"
 
-const EditStream = () => {
-  return <h1>Edit Stream</h1>
+const EditStream = props => {
+  console.log(props)
+  return (
+    <div>
+      <h1>Edit Stream</h1>
+      <h3>ID: {props.match.params.id}</h3>
+      <h4>Title: {props.stream.title}</h4>
+    </div>
+  )
+}
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  }
 }
 
-export default EditStream
+export default connect(mapStateToProps)(EditStream)
