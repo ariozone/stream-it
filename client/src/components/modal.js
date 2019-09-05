1a644b1ee39efa81ab98f5640a4fc92c7c4ebc04
Ario Tamaddoni <ariozone6@gmail.com>
2019-09-04 19:21:27 -0700

    Refactor modal to make it a reusable component.#5

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -1,25 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import history from "../history"
 
 const Modal = props => {
   return ReactDOM.createPortal(
-    <div
-      onClick={() => history.push("/")}
-      className='ui dimmer modals visible active'
-    >
+    <div onClick={props.onDismiss} className='ui dimmer modals visible active'>
       <div
         onClick={e => e.stopPropagation()}
         className='ui standard modal visible active'
       >
-        <div className='header'>Delete Stream</div>
+        <div className='header'>{props.header}</div>
         <div className='content'>
-          <h4>Are you sure you want to delete this stream?</h4>
+          <h4>{props.content}</h4>
         </div>
-        <div className='actions'>
-          <div className='ui red button'>Delete</div>
-          <div className='ui cancel button'>Cancel</div>
-        </div>
+        <div className='actions'>{props.actions}</div>
       </div>
     </div>,
     document.getElementById("modal")
--- client/src/components/streams/DeleteStream.jsx
+++ client/src/components/streams/DeleteStream.jsx
@@ -1,11 +1,23 @@
 import React from "react"
 import Modal from "../modal"
+import history from "../../history"
 
 const DeleteStream = () => {
+  const actions = (
+    <div>
+      <div className='ui red button'>Delete</div>
+      <div className='ui cancel button'>Cancel</div>
+    </div>
+  )
   return (
     <div>
       <h1>Delete Stream</h1>
-      <Modal />
+      <Modal
+        header='Delete Stream'
+        content='Are you sure you want to delete this stream?'
+        actions={actions}
+        onDismiss={() => history.push("/")}
+      />
     </div>
   )
 }
