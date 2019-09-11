831a717128fdaff9c6d12d7950bebaff67ed8347
Ario Tamaddoni <ariozone6@gmail.com>
2019-09-10 17:43:30 -0700

    Enhance the ShowStream class component to fetch the selected stream.#6

--- client/src/components/streams/ShowStream.jsx
+++ client/src/components/streams/ShowStream.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from "react"
+import { getStream } from "../../actions"
+import { connect } from "react-redux"
 
 class ShowStream extends Component {
+  componentDidMount() {
+    this.props.getStream(this.props.match.params.id)
+  }
   render() {
-    return <h1>Show Stream</h1>
+    return (
+      <h1>
+        Show Stream {this.props.stream ? this.props.stream.title : "Loading..."}
+      </h1>
+    )
   }
 }
-
-export default ShowStream
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] }
+}
+export default connect(
+  mapStateToProps,
+  { getStream }
+)(ShowStream)
