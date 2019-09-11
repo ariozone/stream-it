cd78012e345d7520385a87a9d84d0afd4a38754b
Ario Tamaddoni <ariozone6@gmail.com>
2019-09-11 11:30:17 -0700

    Used flv to create a player that gets the stream using route parameter.#6

--- client/src/components/streams/ShowStream.jsx
+++ client/src/components/streams/ShowStream.jsx
@@ -8,15 +8,39 @@ class ShowStream extends Component {
     super(props)
     this.videoRef = React.createRef()
   }
+
   componentDidMount() {
-    this.props.getStream(this.props.match.params.id)
+    const { id } = this.props.match.params
+    this.props.getStream(id)
+    this.createPlayer()
   }
+
+  componentDidUpdate() {
+    this.createPlayer()
+  }
+
+  createPlayer = () => {
+    const { id } = this.props.match.params
+    if (this.flvPlayer || !this.props.stream) return null
+    this.flvPlayer = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`
+    })
+    this.flvPlayer.attachMediaElement(this.videoRef.current)
+    this.flvPlayer.load()
+    //this.flvPlayer.play()
+  }
+
   render() {
     return !this.props.stream ? (
-      <div>Loading</div>
+      <div>Loading...</div>
     ) : (
       <div>
-        <video ref={this.videoRef}></video>
+        <video
+          ref={this.videoRef}
+          style={{ width: "100%" }}
+          controls={true}
+        ></video>
         <h1>{this.props.stream.title}</h1>
         <h3>{this.props.stream.description}</h3>
       </div>
